74327b640f4c57aa209f3bc9fdd9d5a3615bdab9
lradloff <lennart-radloff-studium@outlook.de>
2021-10-17 16:19:46 +0200

    test

--- src/main/java/htw/berlin/webtech/WTProjekt/HelloController.java
+++ src/main/java/htw/berlin/webtech/WTProjekt/HelloController.java
@@ -1,13 +1,27 @@
 package htw.berlin.webtech.WTProjekt;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/")
-    public String index() {
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
         return "Hello World!";
     }
 }
+
+/**
+ @RestController
+ public class HelloController {
+
+ @GetMapping("/")
+ public String index() {
+ return "Hello World!";
+ }
+ }
+ **/
