248f8410ef1bf58ace3e2cec12905c1b2e09fa53
lradloff <lennart-radloff-studium@outlook.de>
2021-10-17 14:13:57 +0200

    first commit

---
+++ src/test/java/htw/berlin/webtech/WTProjekt/WtProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.webtech.WTProjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
