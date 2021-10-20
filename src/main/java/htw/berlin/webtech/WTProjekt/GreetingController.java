629aa9f37d1962cf00107ff191997183dc014de1
Arschnart <lennart-radloff-studium@outlook.de>
2021-10-20 15:49:32 +0200

    Milestone 1

--- src/main/java/htw/berlin/webtech/WTProjekt/GreetingController.java
+++ src/main/java/htw/berlin/webtech/WTProjekt/GreetingController.java
@@ -1,16 +1,31 @@
 package htw.berlin.webtech.WTProjekt;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
+
+/**
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
-        return "Hello World!";
+        return "Hello World!    ";
     }
 }
+**/
