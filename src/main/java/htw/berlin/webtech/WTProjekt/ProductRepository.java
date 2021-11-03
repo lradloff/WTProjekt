3f817eef49e9b6633b78f0036676f95a2e1b02ba
Arschnart <lennart-radloff-studium@outlook.de>
2021-11-03 16:05:19 +0100

    Milestone 2

---
+++ src/main/java/htw/berlin/webtech/WTProjekt/ProductRepository.java
@@ -0,0 +1,9 @@
+package htw.berlin.webtech.WTProjekt;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
--- src/main/java/htw/berlin/webtech/WTProjekt/WtProjektApplication.java
+++ src/main/java/htw/berlin/webtech/WTProjekt/WtProjektApplication.java
@@ -1,10 +1,16 @@
 package htw.berlin.webtech.WTProjekt;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WtProjektApplication {
+
+	//@Autowired
+	//private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WtProjektApplication.class, args);
